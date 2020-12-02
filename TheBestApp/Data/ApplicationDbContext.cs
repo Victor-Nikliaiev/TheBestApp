86be72f23ac89569aa9308dc9de02460d3696425
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2020-12-02 03:29:20 +0200

    Добавьте файлы проекта.

---
+++ TheBestApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheBestApp.Models;
+
+namespace TheBestApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TheBestApp.Models.Person> Person { get; set; }
+    }
+}
